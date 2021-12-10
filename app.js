874fe98cb50031b71bdfd380698db2e3ac1b0baa
bothmane <baq.othmane@outlook.fr>
2021-12-10 16:54:20 +0100

    link friends

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
   session
     .run("MATCH (n:Person) RETURN n")
     .then((result) => {
-      var personArr = [];
+      const personArr = [];
       result.records.forEach((record) => {
         //console.log(record.get(0));
         personArr.push({
@@ -32,9 +32,63 @@ app.get("/", (req, res) => {
           name: record.get(0).properties.name,
         });
       });
-      res.render("index", {
-        persons: personArr,
+
+      session.run("MATCH (n:Location) RETURN n").then((result2) => {
+        const locationArr = [];
+        result2.records.forEach((record) => {
+          locationArr.push(record.get(0).properties);
+        });
+        res.render("index", {
+          persons: personArr,
+          locations: locationArr,
+        });
       });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+//add person
+app.post("/person/add", (req, res) => {
+  const name = req.body.name;
+
+  session
+    .run("CREATE (n:Person {name:$nameP}) RETURN n.name ", { nameP: name })
+    .then((result) => {
+      res.redirect("/");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+//add location
+app.post("/location/add", (req, res) => {
+  const city = req.body.city;
+
+  session
+    .run("CREATE (n:Location {city:$cityP}) RETURN n.name ", { cityP: city })
+    .then((result) => {
+      res.redirect("/");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+//connect friends
+app.post("/friends/connect", (req, res) => {
+  const name1 = req.body.name1;
+  const name2 = req.body.name2;
+
+  session
+    .run(
+      "MATCH (a:Person {name:$name1}), (b:Person {name:$name2}) MERGE (a)-[:FRIEND]->(b)",
+      { name1, name2 }
+    )
+    .then((result) => {
+      res.redirect("/");
     })
     .catch((error) => {
       console.log(error);
